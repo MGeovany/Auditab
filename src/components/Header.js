c40b0305756c5d80052ef2ad5532a95f23ae267f
Geovany <marlongeo1999@gmail.com>
2022-05-20 15:10:25 -0600

    created every component

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,13 +2,8 @@ import React from 'react'
 
 export const Header = () => {
   return (
-    <div className='head' data-testid='head' >
-      <p>
-        Header
-      </p>
-
-      <a href=''>a</a>
-
+    <div>
+     Header
     </div>
   )
 }
