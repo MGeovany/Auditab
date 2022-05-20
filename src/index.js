715ee45e867268937d6bcaf9aad33b8a0b6834a2
Geovany <marlongeo1999@gmail.com>
2022-05-20 15:14:33 -0600

    added some tests

--- src/index.js
+++ src/index.js
@@ -3,13 +3,11 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
-import { Header } from './components/Header'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
     <App />
-    <Header/>
   </React.StrictMode>
 )
 
