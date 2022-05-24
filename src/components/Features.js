c70ccf9b2657f259e951d04864898d56d89da8c1
arl1275 <54734069+arl1275@users.noreply.github.com>
2022-05-23 22:51:04 -0600

    I did some stuff about cards services/features

--- src/components/Features.js
+++ src/components/Features.js
@@ -1,7 +1,24 @@
-import React from 'react'
+import React from 'react';
+import css from "./Features.module.css";
 
-export const Features = () => {
+export const Features = (prop) =>{
+
+
   return (
-    <div>Features</div>
+    <>
+      <div className={css.card_Features}>
+
+        <div>
+          <img src = {prop.Image}></img>
+        </div>   
+
+        <div className= {css.DivF}>
+          <h4 className= {css.Title} >{prop.Title}</h4>
+          <p className={css.longtext}>{prop.LongText}</p>
+        </div>
+
+      </div>
+
+    </>
   )
 }
--- src/components/Services.js
+++ src/components/Services.js
@@ -1,7 +1,27 @@
 import React from 'react'
+import css from "./Services.module.css"
 
-export const Services = () => {
+export const Services = (prop) => {
+
+
   return (
-    <div>Services</div>
+    <>
+      <div className = {css.card_Services}>
+
+        <img src= {prop.Image}/>
+
+        <div className= {css.SerPos}>
+
+          <h4 className={css.SerTitle}>{prop.Title}</h4>
+
+          <p className={css.SerLongText}>{prop.LongText}</p>
+
+          <h7>Read More</h7>
+          
+        </div>
+        
+
+      </div>
+    </>
   )
 }
