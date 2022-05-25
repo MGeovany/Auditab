8dfa39446c8154d65f1a1b76454fb249d77dec95
Geovany <marlongeo1999@gmail.com>
2022-05-24 22:27:57 -0600

    finished all the login & sign up styles

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-import Login from './components/Login'
+import { Signup } from "./components/Signup";
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Login />
+    <Signup />
   </React.StrictMode>
-)
-
+);
